b39dcc90a918aeb10f8e5ba6fabfcbd311097af2
pakuland3 <pakuland_1@naver.com>
2024-02-13 12:07:12 +0900

    created 1005

--- c++/1005.cpp
+++ c++/1005.cpp
@@ -5,42 +5,35 @@ int t;
 
 void solve(){
     int n,k;
-    vector<int> v[1001];
-    vector<int> h[1001];
-    vector<int> starts;
+    vector<int> v[1001]; //next vector
+    vector<int> h[1001]; //req vector
+    int reqarr[1001];
+    memset(reqarr,0,sizeof(reqarr));
     int dp[1001];
     int arr[1001];
     int tmp[2];
     int target;
+    queue<int> q;
     cin >> n >> k;
     for(int i=1;i<=n;i++) cin >> arr[i];
     for(int i=0;i<k;i++){
         cin >> tmp[0] >> tmp[1];
-        v[tmp[1]].push_back(tmp[0]);
-        h[tmp[0]].push_back(tmp[1]);
+        v[tmp[0]].push_back(tmp[1]);
+        h[tmp[1]].push_back(tmp[0]);
+        reqarr[tmp[1]]++;
     }
     cin >> target;
-    for(int i=1;i<=n;i++){
-        if(v[i].size()==0){
-            starts.push_back(i);
-        }
-    }
-    queue<int> q;
-    bool vis[1001];
-    for(auto elem:starts){
-        q.push(elem);
-        vis[elem]=true;
-    }
+    for(int i=1;i<=n;i++){if(reqarr[i]==0) q.push(i);}
     while(!q.empty()){
         int x=q.front();
-        if(!vis[x]) vis[x]=true;
         q.pop();
         dp[x]=arr[x];
         int maxi=0;
-        for(int i=0;i<v[x].size();i++) maxi=max(maxi,dp[v[x][i]]);
+        for(int i=0;i<h[x].size();i++) maxi=max(maxi,dp[h[x][i]]);
         dp[x]+=maxi;
-        for(int i=0;i<h[x].size();i++){
-            if(!vis[h[x][i]]) q.push(h[x][i]);
+        for(int i=0;i<v[x].size();i++){
+            reqarr[v[x][i]]--;
+            if(reqarr[v[x][i]]==0) q.push(v[x][i]);
         }
     }
     cout << dp[target] << '\n';
