c0eee43f1013f4da23209277515d165a81724214
pakuland3 <pakuland_1@naver.com>
2024-02-15 20:11:24 +0900

    one commit

--- c++/12851.cpp
+++ c++/12851.cpp
@@ -3,7 +3,6 @@ using namespace std;
 
 int dp[100001][2];
 int n,k;
-int dx[]={1,-1};
 
 void bfs(){
     queue<int> q;
@@ -12,25 +11,15 @@ void bfs(){
     while(!q.empty()){
         int now=q.front();
         q.pop();
-        for(int i=0;i<2;i++){
-            int next=now+dx[i];
+        for(int next:{now-1,now+1,now*2}){
             if(-1<next && next<100001){
                 if(dp[next][0]>dp[now][0]+1){
                     dp[next][0]=dp[now][0]+1;
                     dp[next][1]=dp[now][1]; 
                     q.push(next);
-                } //definitely not searching exist
+                }
                 else if(dp[next][0]==dp[now][0]+1) dp[next][1]+=dp[now][1];
             }
-        }
-        int next=now*2;
-        if(next<100001){
-            if(dp[next][0]>dp[now][0]+1){
-                dp[next][0]=dp[now][0]+1;
-                dp[next][1]=dp[now][1];
-                q.push(next);
-            }
-            else if(dp[next][0]==dp[now][0]+1) dp[next][1]+=dp[now][1];
         }
     }
     cout << dp[k][0] << '\n' << dp[k][1];
