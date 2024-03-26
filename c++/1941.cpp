68fdc7c87d4b9b57ad6659b1de9df46b5b292aee
pakuland3 <pakuland_1@naver.com>
2024-03-26 23:37:17 +0900

    0327 CODE

---
+++ c++/1941.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dx[]={1,-1,0,0};
+int dy[]={0,0,1,-1};
+char f[5][5];
+bool vis[5][5]={0,};
+
+void solve(int x, int y, int cnt, int p[2]){
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    for(int i=0;i<25;i++) cin >> f[i/5][i%5];
+    int 
+    solve(0,0,0,);
+    return 0;
+}
