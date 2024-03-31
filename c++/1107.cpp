840c1052e2804286d6d4134f9389143d262767cb
pakuland3 <pakuland_1@naver.com>
2024-03-31 18:30:25 +0900

    0331

--- c++/1107.cpp
+++ c++/1107.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m,ans,sl;
-string s;
-vector<int> arr,nums;
+int n,m,s=0,ans;
+vector<int> arr;
 
-void solve(int cnt,string l, bool b, int zerocnt){
-    if(!b) ans=min(ans,n+1);
-    else ans=min(ans,abs(stoi(l)-n)+cnt-zerocnt);
-    if(cnt==7) return;
+void solve(int cnt, string l){
+    if(cnt!=0){
+        ans=min(ans,cnt+abs(stoi(l)-n));
+        if(l[0]=='0') return;
+    }
+    if(cnt==6) return;
     for(int i=0;i<arr.size();i++){
         l+=arr[i]+'0';
-        if(arr[i]==0 && !b) solve(cnt+1,l,0,zerocnt+1);
-        else solve(cnt+1,l,1,zerocnt);
+        solve(cnt+1,l);
         l.erase(l.length()-1);
     }
     return;
@@ -20,16 +20,14 @@ void solve(int cnt,string l, bool b, int zerocnt){
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> s >> m; n=stoi(s); ans=abs(n-100); sl=s.length();
-    for(int i=0;i<m;i++){int inn; cin >> inn; arr.push_back(inn);} 
-    if(s=="100"){cout << 0; return 0;}
-    sort(arr.begin(),arr.end()); int s=0;
+    cin >> n >> m; ans=abs(100-n);
+    for(int i=0;i<m;i++){int inn; cin >> inn; arr.push_back(inn);}sort(arr.begin(),arr.end());
     for(int i=0;i<10;i++){
         if(s<arr.size() && i==arr[s]) s++;
         else arr.push_back(i);
     }
     for(int i=0;i<m;i++) arr.erase(arr.begin());
-    solve(0,"",0,0);
+    solve(0,"");
     cout << ans;
     return 0;
 }
