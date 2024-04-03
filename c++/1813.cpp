d84d3a6fe9e5d247498b0cc6ea5b4324ee85e6c8
pakuland3 <pakuland_1@naver.com>
2024-04-03 23:25:22 +0900

    0403

---
+++ c++/1813.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,tmp,ans=-1;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cin >> n; vector<int> cnt(n+1,0);
+    for(int i=0;i<n;i++){
+        cin >> tmp;
+        if(tmp>n) continue;
+        cnt[tmp]++;
+    }
+    for(int i=0;i<=n;i++){
+        if(cnt[i]==i) ans=i;
+    }
+    if(ans==-1) cout << -1;
+    else cout << ans;
+    return 0;
+}
