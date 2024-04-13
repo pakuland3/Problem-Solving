514b5c03350e8dabb867af5c66951343ebad65df
pakuland3 <pakuland_1@naver.com>
2024-04-14 01:15:51 +0900

    0414

--- c++/1474.cpp
+++ c++/1474.cpp
@@ -1,11 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n,m;
+int n,m,l=0;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
-    for()
+    vector<string> arr(n);
+    for(int i=0;i<n;i++){cin >> arr[i];l+=arr[i].length();}
+    int left=m-l;
+    if(left%(n-1)==0) for(string s:arr){
+        cout << s;
+        for(int i=0;i<left/(n-1);i++) cout << '_';
+    }
+    else{
+        
+    }
     return 0;
 }
