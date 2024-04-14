f7b9c66175ad99db69a43e406381d129b2cfc82a
pakuland3 <pakuland_1@naver.com>
2024-04-14 17:12:46 +0900

    0414

---
+++ c++/1069.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int x,y,d,t;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cout.precision(9); cout << fixed;
+    cin >> x >> y >> d >> t;
+    double distance=sqrt(x*x+y*y);
+    
+    return 0;
+}
