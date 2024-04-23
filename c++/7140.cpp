71249b33212d50c0feb05094336847ac971e9250
pakuland3 <pakuland_1@naver.com>
2024-04-23 10:32:57 +0900

    0423

---
+++ c++/7140.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    cout << "101\n1 1 2\n";
+    for(int i=0;i<100;i++) cout << "0\n";
+    cout << "1\n0 1";
+    return 0;
+}
