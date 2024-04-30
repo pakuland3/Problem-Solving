086741bbf0d32a7f01d299d050b2bdedc16d31a2
pakuland3 <pakuland_1@naver.com>
2024-04-30 23:24:25 +0900

    to do: learn locality property definition

--- c++/10412.cpp
+++ c++/10412.cpp
@@ -1,46 +1,54 @@
-//UNION FIND PROBLEM
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
 
-// #include <bits/stdc++.h>
-// using namespace std;
+int m,n,k,tmp;
+bool b=0;
+vector<vector<int>> arr;
+vector<int> parent;
 
-// int m,n,k,c;
-// vector<vector<int>> arr;
-// bool ck[100]={0,},ans=0;
+void unionParent(int a,int b){
+    parent[b]=a;
+}
 
-// void solve(int cnt,int ind){
-//     if(ans) return;
-//     if(cnt==k){
-//         bool t=1;
-//         for(int i=0;i<n;i++) if(!ck[i]) t=0;
-//         if(t) ans=1;
-//         return;
-//     }
-//     bool b=0;
-//     for(int i=ind+1;i<n;i++){
-//         ck[i]=1;
-//         solve(cnt+1,i);
-//         ck[i]=0;
-//         if(!b) b=1;
-//     }
-//     if(!b){
-//         bool t=1;
-//         for(int i=0;i<n;i++) if(!ck[i]) t=0;
-//         if(t) ans=1;
-//         return;
-//     }
-// }
+void solve(int backnode,int ind,int st,int cnt){
+    if(b) return;
+    if(cnt==n){cout << "yes"; b=1; return;}
+    if(st==k) return;
+    for(int i=ind+1;i<m;i++){
+        if(arr[i].size()==0) continue;
+        if(backnode!=-1){
+            vector<int> tmp; for(int j=0;j<arr[i].size();j++){
+                if(parent[arr[i][j]]!=backnode) tmp.push_back(arr[i][j]);
+            }
+            for(int to:tmp) unionParent(backnode,to);
+            solve(backnode,i,st+1,cnt+tmp.size());
+            for(int j=0;j<tmp.size();j++) parent[tmp[j]]=tmp[j];
+        }
+        else{
+            backnode=arr[i][0];
+            for(int j=1;j<arr[i].size();j++) unionParent(backnode,arr[i][j]);
+            solve(backnode,i,1,arr[i].size());
+            backnode=-1;
+            for(int j=0;j<n;j++) parent[j]=j;
+        }
+    }
+    return;
+}
 
-// int main(){
-//     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-//     cin >> m >> n >> k;
-//     for(int i=0;i<m;i++){
-//         arr.push_back(vector<int> ());
-//         for(int j=0;j<n;j++){
-//             cin >> c; if(c==0) arr.back().push_back(j);
-//         }
-//         if(arr.back().size()==0) arr.pop_back();
-//     }
-//     for(int i=0;i<arr.size();i++){ck[i]=1;solve(1,i);ck[i]=0;}
-//     if(ans) cout << "yes";
-//     else cout << "no";
-// }
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("input.txt","r",stdin);
+    cin >> m >> n >> k;
+    arr.resize(m,vector<int> ());
+    for(int i=0;i<m;i++) for(int j=0;j<n;j++){
+        cin >> tmp;
+        if(tmp==0) arr[i].push_back(j);
+    }
+    parent.resize(n);
+    for(int i=0;i<n;i++) parent[i]=i;
+    solve(-1,-1,0,0);
+    if(!b) cout << "no";
+    return 0;
+}
