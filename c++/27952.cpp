c3423f4909a1e1002001ace63b53c0241baf504c
pakuland3 <pakuland_1@naver.com>
2024-05-10 15:24:21 +0900

    0510

--- c++/27952.cpp
+++ c++/27952.cpp
@@ -4,9 +4,20 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n,x;
+    ll n,x,ans=0,d=0,cnt;
     cin >> n >> x;
-    vector<int> w(n);
-    for(int &a:w) cin >> a;
+    vector<ll> a(n),b(n),c(n);
+    for(ll &t:a) cin >> t;
+    for(ll &t:b) cin >> t;
+    c[n-1]=a[n-1];
+    for(int i=n-2;i>-1;i--) c[i]=max(c[i+1]-b[i+1],a[i]);
+    for(int i=0;i<n;i++){
+        d+=b[i];
+        if(d<c[i]){cout << -1; return 0;}
+        cnt=(d-c[i])/x;
+        ans+=cnt;
+        d-=cnt*x;
+    }
+    cout << ans;
     return 0;
 }
