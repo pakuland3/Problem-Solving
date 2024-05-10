0cc7eb8bbc33ef97be2f745f1936e64d09943065
pakuland3 <pakuland_1@naver.com>
2024-05-10 13:50:49 +0900

    0510

---
+++ c++/27952.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n,x;
+    cin >> n >> x;
+    vector<int> w(n);
+    for(int &a:w) cin >> a;
+    return 0;
+}
