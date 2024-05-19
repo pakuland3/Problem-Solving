2408a97ffb1df6eef577602e23a1961d41193217
pakuland3 <pakuland_1@naver.com>
2024-05-19 18:08:35 +0900

    0519

--- c++/16496.cpp
+++ c++/16496.cpp
@@ -4,12 +4,16 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int r,c,k;
-    cin >> r >> c >> k;
-    if(r*c%2!=0){
-        if(k>1) cout << 0;
-        else cout << 1;
+    int n;
+    cin >> n;
+    vector<string> arr(n);
+    for(string &a:arr) cin >> a;
+    sort(arr.begin(),arr.end(),[](string &a, string &b)->bool{
+        return stoul(a+b)>stoul(b+a);
+    });
+    for(int i=0;i<n;i++){
+        if(i==0 && stoul(arr[i])==0){cout << 0; return 0;}
+        cout << arr[i];
     }
-    else cout << 1;
     return 0;
 }
