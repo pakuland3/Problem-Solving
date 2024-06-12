ae612a997ece842ab212a406b344e125434a39c2
pakuland3 <pakuland_1@naver.com>
2024-06-12 16:50:39 +0900

    0612

--- c++/13322.cpp
+++ c++/13322.cpp
@@ -6,14 +6,6 @@ int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     string s;
     cin >> s;
-    int n=s.length();
-    vector<pair<string, int>> a(s.length());
-    for(int i=0;i<n;i++){
-        a[i].second=i;
-        a[i].first=s;
-        s.erase(s.begin());
-    }
-    sort(a.begin(),a.end());
-    for(int i=0;i<n;i++) cout << a[i].second << '\n';
+    for(int i=0;i<s.length();i++) cout << i << '\n';
     return 0;
 }
