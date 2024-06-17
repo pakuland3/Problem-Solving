ca6a4779877ba0e9a6de55302d143920db39555c
pakuland3 <pakuland_1@naver.com>
2024-06-17 14:53:23 +0900

    0617

--- c++/14427.cpp
+++ c++/14427.cpp
@@ -1,33 +1,50 @@
 #include <bits/stdc++.h>
 #define SIZE 1<<21
+#define MAX (int)2e9
 typedef long long ll;
 using namespace std;
 
-vector<ll> tree(SIZE,0);
+vector<int> tree(SIZE,MAX);
+vector<int> idx(SIZE,0);
 
-void update(int X, ll V, int node, int S, int E){
+void update(int X, int V, int node, int S, int E){
     if(S==E){
-        tree[X]=V;
+        tree[node]=V;
+        idx[node]=X;
         return;
     }
     int MID=(S+E)/2;
     if(X<=MID) update(X,V,2*node,S,MID);
     else update(X,V,2*node+1,MID+1,E);
-    tree[node]=min(tree[2*node],tree[2*node+1]);
+    if(tree[2*node]<tree[2*node+1]){
+        tree[node]=tree[2*node];
+        idx[node]=idx[2*node];
+    }
+    else{
+        tree[node]=tree[2*node+1];
+        if(tree[2*node]!=tree[2*node+1]) idx[node]=idx[2*node+1];
+        else idx[node]=idx[2*node];
+    }
 }
 
-int getMin(){
-    
+int getMini(){
+    return idx[1];
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n,tmp;
+    int n,ini,m;
     cin >> n;
     for(int i=1;i<=n;i++){
-        cin >> tmp;
-        update(i,tmp,1,1,n);
+        cin >> ini;
+        update(i,ini,1,1,n);
     }
-    
+    cin >> m;
+    while(m--){
+        cin >> ini;
+        int i,v;
+        if(ini==2) cout << getMini() << '\n';
+        else{cin >> i >> v; update(i,v,1,1,n);}
+    }
     return 0;
 }
