b2ae748a9404c62e1fbea3a7930040a3e938945b
pakuland3 <pakuland_1@naver.com>
2024-06-19 19:28:01 +0900

    0619

--- c++/5885.cpp
+++ c++/5885.cpp
@@ -19,5 +19,19 @@ int main(){
         a[i].idx=i;
     }
     sort(a.begin(),a.end());
+    int cur=1;
+    int ans=1;
+    vector<bool> done(n,1);
+    for(int i=0;i<n;i++){
+        int ix=a[i].idx;
+        done[ix]=0;
+        bool l=(ix>0 && done[ix-1]);
+        bool r=(ix<n-1 && done[ix+1]);
+        if(l && r) cur++;
+        else if(!l && !r) cur--;
+        if(i==n-1 || a[i]<a[i+1]) ans=max(ans,cur);
+
+    }
+    cout << ans;
     return 0;
 }
