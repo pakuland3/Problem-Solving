5869e0b6605a62357ca8b7631681a183cd9c772f
pakuland3 <pakuland_1@naver.com>
2024-06-21 17:51:37 +0900

    0621

--- c++/1300.cpp
+++ c++/1300.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    ll x,y;
-    cin >> x >> y;
-    cout << "2\n" << x*(y-1) << '\n' << y-x*y;
+    ll n;
+    vector<ll> p;
+    for(int i=1;i<=n;i++){
+        p.push_back(i*i);
+    }
+    cin >> n;
+    ll s=1,e=n*n;
+    while(s<e){
+        ll mid=(s+e)/2;
+        auto it=upper_bound(p.begin(),p.end(),mid);
+        if(*it!=1) it--;
+        ll cnt=sqrt(*it)+2*(mid-sqrt(*it));;
+        if(mid==*it) cnt-=2;
+    }
+    cout << e;
     return 0;
 }
