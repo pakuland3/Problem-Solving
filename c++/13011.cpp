835a2c18374b04df664f6eecc672100729cbf0e7
pakuland3 <pakuland_1@naver.com>
2024-07-02 22:21:04 +0900

    0702

--- c++/13011.cpp
+++ c++/13011.cpp
@@ -5,31 +5,21 @@ using namespace std;
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cout.precision(9);
+    cout.precision(10);
     cout << fixed;
     int n;
-    ld s,e,eps=pow(10,-12);
     cin >> n;
-    vector<ld> c(n),w(n);
-    for(ld &t:c) cin >> t;
-    cin >> w[0];
-    s=eps;
-    e=1e6;
-    for(int i=1;i<n;i++) cin >> w[i];
-    while(e-s>=eps){
-        ld a=s+(e-s)/3;
-        ld b=e-(e-s)/3;
-        ld difofa=0;
-        ld difofb=0;
-        for(int i=0;i<n;i++){
-            difofa+=abs(c[i]*a-w[i]);
-            difofb+=abs(c[i]*b-w[i]);
-        }
-        if(difofa>difofb) s=a;
-        else e=b;
+    vector<double> c(n),w(n);
+    for(double &a:c) cin >> a;
+    for(double &a:w) cin >> a;
+    double ans=0;
+    for(int i=0;i<n;i++) ans+=abs(c[i]*w[0]/c[0]-w[i]);
+    for(int i=1;i<n;i++){
+        double d=w[i]/c[i];
+        double tmp=0;
+        for(int i=0;i<n;i++) tmp+=abs(d*c[i]-w[i]);
+        ans=min(ans,tmp);
     }
-    ld ans=0;
-    for(int i=0;i<n;i++) ans+=abs(c[i]*s-w[i]);
     cout << ans;
     return 0;
 }
