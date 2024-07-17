0e729d48c1264904b7d756df50ea2c2a9ce0f485
pakuland3 <pakuland_1@naver.com>
2024-07-17 21:34:57 +0900

    0717

---
+++ c++/6549.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+int a[100001];
+
+ll solve(int l, int r){
+    if(l==r) return a[l];
+    int s=(l+r)/2,e=s+1;
+    ll tmp=min(a[s],a[e])*2;
+    
+    return max(tmp,max(solve(l,s),solve(e,r)));
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    while(1){
+        cin >> n;
+        if(n==0) break;
+        for(int i=0;i<n;i++) cin >> a[i];
+        cout << solve(0,n-1) << '\n';
+    }
+    return 0;
+}
