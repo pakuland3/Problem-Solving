86ff8ef1045ba89adce8bc43dc4e11f79ddc52da
pakuland3 <pakuland_1@naver.com>
2024-07-18 12:38:22 +0900

    0718

--- c++/6549.cpp
+++ c++/6549.cpp
@@ -3,20 +3,52 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-int a[100001];
+ll a[100001];
 
 ll solve(int l, int r){
     if(l==r) return a[l];
-    int s=(l+r)/2,e=s+1;
-    ll tmp=min(a[s],a[e])*2;
-    while(s!=l || e!=r){
-        
+    int mid=(l+r)/2;
+    ll s=(l+r)/2,e=s+1;
+    ll curh;
+    if(a[s]>a[e]){
+        curh=a[s];
+        e--;
     }
-    return max(tmp,max(solve(l,s),solve(e,r)));
+    else{
+        curh=a[e];
+        s++;
+    }
+    ll tmp=curh;
+    while(s!=l || e!=r){
+        if(s==l || e==r){
+            if(s==l){
+                e++;
+                curh=min(curh,a[e]);
+            }
+            else{
+                s--;
+                curh=min(curh,a[s]);
+            }
+            tmp=max(tmp,curh*(e-s+1));
+            continue;
+        }
+        if(a[s-1]>a[e+1]){
+            s--;
+            curh=min(curh,a[s]);
+            tmp=max(tmp,curh*(e-s+1));
+        }
+        else{
+            e++;
+            curh=min(curh,a[e]);
+            tmp=max(tmp,curh*(e-s+1));
+        }
+    }
+    return max(tmp,max(solve(l,mid),solve(mid+1,r)));
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    // freopen("in","r",stdin);
     int n;
     while(1){
         cin >> n;
