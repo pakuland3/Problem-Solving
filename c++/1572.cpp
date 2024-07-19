2e236e9cf3c412ad90e2b8ae87e8c06b64c63214
pakuland3 <pakuland_1@naver.com>
2024-07-20 00:33:02 +0900

    0719-0720

--- c++/1572.cpp
+++ c++/1572.cpp
@@ -4,7 +4,7 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-int tree[4*SIZE];
+int tree[4*SIZE],arr[250001];
 
 void update(int X, int V, int node, int S, int E){
     if(S==E){
@@ -20,22 +20,25 @@ void update(int X, int V, int node, int S, int E){
 ll query(int cnt, int node, int S, int E){
     if(S==E) return S;
     int MID=(S+E)/2;
-    if(tree[2*node+1]>cnt) return query(cnt,2*node+1,S,E);
-    return query(cnt,2*node,S,E);
+    if(tree[2*node]>=cnt) return query(cnt,2*node,S,MID);
+    return query(cnt-tree[2*node],2*node+1,MID+1,E);
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    int n,k,a;
+    int n,k;
     cin >> n >> k;
     for(int i=0;i<k;i++){
-        cin >> a;
-        update(a,1,1,1,SIZE);
+        cin >> arr[i];
+        update(arr[i],1,1,0,SIZE);
     }
-    ll ans=query((k+1)/2,1,1,SIZE);
-    for(int i=1;i<n+1-k;i++){
-        cin >> a;
-        update(a,-1,1,1,SIZE);
+    ll ans=query((k+1)/2,1,0,SIZE);
+    for(int i=1;i<n-k+1;i++){
+        cin >> arr[k+i-1];
+        update(arr[i-1],-1,1,0,SIZE);
+        update(arr[k+i-1],1,1,0,SIZE);
+        ans+=query((k+1)/2,1,0,SIZE);
     }
+    cout << ans;
     return 0;
 }
