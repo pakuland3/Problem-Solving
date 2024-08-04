cb289b8695c25a303fc5cb1b75cbd8a943fab513
pakuland3 <pakuland_1@naver.com>
2024-08-04 13:38:15 +0900

    0804

--- c++/10892.cpp
+++ c++/10892.cpp
@@ -3,57 +3,27 @@ typedef long long ll;
 using namespace std;
 
 struct poi{
-    double x,y;
-};
-
-struct g{
-    int a,b,c;
-};
-
-struct info{
-    bool done=0;
-    int c;
+    int x,y;
+    int n;
+    bool operator<(poi &r){
+        if(x==r.x) return y<r.y;
+        return x<r.x;
+    }
 };
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     int n;
     cin >> n;
-    vector<bool> done(3*n,0);
-    vector<info> isCon(3*n);
-    vector<g> ans;
-    vector<poi> p(3*n);
-    priority_queue<pair<double,pair<int,int>>,vector<pair<double,pair<int,int>>>,greater<pair<double,pair<int,int>>>> pq;
+    vector<poi> a(3*n);
     for(int i=0;i<3*n;i++){
-        double x,y;
-        cin >> x >> y;
-        for(int j=0;j<i;j++) pq.push({pow(p[j].x-x,2)+pow(p[j].y-y,2),{i,j}});
+        a[i].n=i;
+        cin >> a[i].x >> a[i].y;
     }
-    while(!pq.empty()){
-        int a=pq.top().second.first;
-        int b=pq.top().second.second;
-        pq.pop();
-        if(done[a] || done[b]) continue;
-        if(isCon[a].done && isCon[b].done) continue;
-        if(isCon[a].done){
-            ans.push_back({a,b,isCon[a].c});
-            done[a]=1;
-            done[b]=1;
-            done[isCon[a].c]=1;
-        }
-        else if(isCon[b].done){
-            ans.push_back({a,b,isCon[b].c});
-            done[a]=1;
-            done[b]=1;
-            done[isCon[b].c]=1;
-        }
-        else{
-            isCon[a].done=1;
-            isCon[b].done=1;
-            isCon[a].c=b;
-            isCon[b].c=a;
-        }
+    sort(a.begin(),a.end());
+    for(int i=0;i<3*n;i++){
+        cout << a[i].n+1 << ' ';
+        if(i%3==2) cout << '\n';
     }
-    for(auto &g:ans) cout << g.a+1 << ' ' << g.b+1 << ' ' << g.c+1 << '\n';
     return 0;
 }
