3fd97562df90b130b41889ac9866664caf05e51c
pakuland3 <pakuland_1@naver.com>
2024-08-10 16:13:02 +0900

    finally catch how to solve kind of this problem 0810

--- c++/22336.cpp
+++ c++/22336.cpp
@@ -2,37 +2,19 @@
 typedef long long ll;
 using namespace std;
 
-struct line{
-    int a,b,c,d;
-};
-
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    freopen("xor1.in","r",stdin);
+    freopen("../tmp/tmp-folder/xor6.in","r",stdin);
     freopen("output.txt","w",stdout);
-    vector<line> ans;
-    int n,inn;
+    int n;
     cin >> n;
-    int cnt=0;
-    vector<vector<int>> a(n+1,vector<int> (n+1));
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            cin >> a[i][j];
-        }
-    }
-    for(int i=1;i<=n;i++){
-        int st=1;
-        for(int j=1;j<=n;j++){
-            if(j>1 && !a[j][i] && a[j-1][i]){
-                ans.push_back({i,i,st,j-1});
-            }
-            else if(!a[j][i]) st=j+1;
-        }
+    bool a;
+    vector<pair<int,int>> ans;
+    for(int i=1;i<=n;i++) for(int j=1;j<=n;j++){
+        cin >> a;
+        if(a) ans.push_back({i,j});
     }
     cout << ans.size() << '\n';
-    for(int i=0;i<ans.size();i++){
-        cout << ans[i].a << ' ' << ans[i].b << ' ' << ans[i].c << ' ' << ans[i].d << '\n';
-    }
-    // cout << cnt;
+    for(auto &p:ans) cout << p.second << ' ' << p.second << ' ' << p.first << ' ' << p.first << '\n';
     return 0;
 }
