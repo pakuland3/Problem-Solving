d5117bbb243dedd5479fe759770d79a782151bef
pakuland3 <pakuland_1@naver.com>
2024-08-23 18:10:23 +0900

    0823

--- c++/11003.cpp
+++ c++/11003.cpp
@@ -41,5 +41,23 @@
 //     return 0;
 // }
 
-// deque
+// priority_queue
 
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+    int n,l;
+    cin >> n >> l;
+    for(int i=0;i<n;i++){
+        int a;
+        cin >> a;
+        pq.push({a,i});
+        while(pq.top().second<i-l+1) pq.pop();
+        cout << pq.top().first << ' ';
+    }
+    return 0;
+}
