ba65f4fa9bae8413bd84d18c24fb05835904b966
pakuland3 <pakuland_1@naver.com>
2024-08-29 15:59:37 +0900

    0829

--- c++/1114.cpp
+++ c++/1114.cpp
@@ -2,31 +2,51 @@
 typedef long long ll;
 using namespace std;
 
-int l,k,c,cut;
-int x[10000];
+int l,k,c,cut,ans,idx=2;
+int x[10003];
 
 bool solve(int mid){
-    int back=0;
     int cnt=0;
-    bool vis[10001]={0,};
-    while(back<l){
-        
+    int len=0;
+    for(int i=idx-2;i>-1;i--){
+        if(x[i+1]-x[i]>mid) return 0;
+        if(len>0){
+            if(len+x[i+1]-x[i]<mid) len+=x[i+1]-x[i];
+            else{
+                if(len+x[i+1]-x[i]==mid){
+                    len=0;
+                    if(i!=0){
+                        cut=x[i];
+                        cnt++;
+                    }
+                }
+                else{
+                    len=x[i+1]-x[i];
+                    cut=x[i+1];
+                    cnt++;
+                }
+            }
+        }
+        else len=x[i+1]-x[i];
     }
+    if(cnt<c) cut=x[1];
     if(cnt>c) return 0;
+    ans=cut;
     return 1;
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     cin >> l >> k >> c;
-    for(int i=0;i<k;i++) cin >> x[i];
-    sort(x,x+k);
+    for(int i=1;i<=k;i++) cin >> x[i];
+    sort(x+1,x+k+1); for(int i=2;i<=k;i++) if(x[i]!=x[i-1]) x[idx++]=x[i];
+    x[idx++]=l;
     int s=1,e=l;
     while(s<=e){
         int mid=(s+e)/2;
-        if(solve(mid)) s=mid+1;
-        else e=mid-1;
+        if(solve(mid)) e=mid-1;
+        else s=mid+1;
     }
-    cout << e << ' ' << cut;
+    cout << s << ' ' << ans;
     return 0;
 }
