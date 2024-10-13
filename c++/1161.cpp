74434b8444246001afe5b8516fc66c53f08a0594
pakuland3 <pakuland_1@naver.com>
2024-10-13 16:13:08 +0900

    pause on 1013

---
+++ c++/1161.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define SIZE 20001
+typedef long long ll;
+using namespace std;
+
+int tree[SIZE*4];
+int lazy[SIZE*4];
+
+void update_lazy(){
+
+}
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    
+    return 0;
+}
