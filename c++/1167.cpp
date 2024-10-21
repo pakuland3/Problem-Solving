02f9b4d071050e0a8692a94fd4a545c57b26901a
pakuland3 <pakuland_1@naver.com>
2024-10-21 16:44:28 +0900

    1021

--- c++/1167.cpp
+++ c++/1167.cpp
@@ -4,71 +4,68 @@
 typedef long long ll;
 using namespace std;
 
-vector<vector<pair<int,ll>>> graph;
-queue<pair<int,ll>> q;
+int d[100001],c[100001];
 bool vis[100001];
-int cnt[100001];
-ll res=0;
 
-void bfs(int a){
-    q.push({a,0});
+vector<vector<pair<int,int>>> graph;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int v;
+    cin >> v;
+    graph.resize(v+1);
+    int to,cost;
+    for(int i=0;i<v;i++){
+        int start;
+        cin >> start;
+        while(1){
+            cin >> to;
+            if(to==-1) break;
+            cin >> cost;
+            c[to]++;
+            c[start]++;
+            graph[start].push_back({to,cost});
+        }
+    }
+    queue<int> q;
+    for(int i=1;i<=v;i++){
+        if(c[i]==2){
+            q.push(i);
+            vis[i]=1;
+            d[i]=0;
+            break;
+        }
+    }
     while(!q.empty()){
-        vis[q.front().FI]=1;
-        ll cur=q.front().SE;
-        res=max(res,cur);
-        int a=q.front().FI;
+        int a=q.front();
         q.pop();
         for(int i=0;i<graph[a].size();i++){
             int na=graph[a][i].FI;
             if(vis[na]) continue;
-            if(cnt[na]==1 && cur<graph[a][i].SE){
-                res=max(res,cur+graph[a][i].SE);
-                cur=0;
-                vis[a]=0;
-                q.push({na,0});
-            }
-            else q.push({na,cur+graph[a][i].SE});
+            vis[na]=1;
+            d[na]=d[a]+graph[a][i].SE;
+            q.push(na);
         }
     }
-}
-
-void dfs(int a, ll cur){
-    vis[a]=1;
-    res=max(res,cur);
-    for(int i=0;i<graph[a].size();i++){
-        int na=graph[a][i].FI;
-        if(vis[na]) continue;
-        if(cnt[na]==1 && cur<graph[a][i].SE){
-            res=max(res,cur+graph[a][i].SE);
-            vis[a]=0;
-            dfs(na,0);
-        }
-        else dfs(na,cur+graph[a][i].SE);
-    }
-    vis[a]=0;
-}
-
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    // freopen("input","r",stdin);
-    int n;
-    cin >> n;
-    graph.resize(n+1);
-    for(int i=1;i<=n;i++){
-        cin >> i;
-        int to;
-        while((cin >> to) && to!=-1){
-            cnt[to]++;
-            int c;
-            cin >> c;
-            graph[i].push_back({to,c});
+    priority_queue<pair<int,int>> pq;
+    for(int i=1;i<=v;i++) pq.push({d[i],i});
+    pair<int,int> a=pq.top();
+    memset(vis,0,sizeof(vis));
+    q.push(a.SE);
+    d[a.SE]=0;
+    vis[a.SE]=1;
+    while(!q.empty()){
+        int aa=q.front();
+        q.pop();
+        for(int i=0;i<graph[aa].size();i++){
+            int na=graph[aa][i].FI;
+            if(vis[na]) continue;
+            vis[na]=1;
+            d[na]=d[aa]+graph[aa][i].SE;
+            q.push(na);
         }
     }
-    for(int i=1;i<=n;i++) 
-        if(cnt[i]==1){
-            dfs(i,0);
-            break;
-        }
-    cout << res;
+    for(int i=1;i<=v;i++) pq.push({d[i],i});
+    cout << pq.top().FI;
     return 0;
 }
