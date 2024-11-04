ecb7d69415da1c1ece31ad32a3e75c00fa810fd5
pakuland3 <pakuland_1@naver.com>
2024-11-04 17:32:58 +0900

    1104

--- c++/32294.cpp
+++ c++/32294.cpp
@@ -1,48 +1,43 @@
 #include <bits/stdc++.h>
-#define FI first
-#define SE second
-#define SIZE 200001
+#define INF 200000000001
+#define F first
+#define S second
+#define pli pair<ll,int>
 typedef long long ll;
 using namespace std;
 
-int dx[]={-1,1};
-int n;
-int a[SIZE],cnt[SIZE];
-ll b[SIZE],dp[SIZE];
-bool vis[SIZE];
-
-ll solve(int x){
-    if(n<x || x<1) return 0;
-    if(dp[x]!=0) return dp[x];
-    vis[x]=1;
-    for(int i=0;i<2;i++){
-        int nx=x+a[x]*dx[i];
-        if(1<=nx && nx<=n && vis[nx]) continue;
-        ll t=solve(nx);
-        dp[x]=(dp[x]==0?t+b[x]:min(dp[x],t+b[x]));
-    }
-    vis[x]=0;
-    return dp[x];
-}
-
 int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL);
-    cin >> n;
-    for(int i=1;i<=n;i++){
-        cin >> a[i];
-        for(int j=0;j<2;j++){
-            int nx=i+dx[j]*a[i];
-            if(n<nx || nx<1) continue;
-            cnt[nx]++;
-        }
-    }
-    for(int i=1;i<=n;i++) cin >> b[i];
-    priority_queue<pair<int,int>,vector<pair<int,int>>,less<pair<int,int>>> pq;
-    for(int i=1;i<=n;i++) pq.push({cnt[i],i});
-    for(int i=1;i<=n;i++){
-        dp[pq.top().SE]=solve(pq.top().SE);
-        pq.pop();
-    }
-    for(int i=1;i<=n;i++) cout << dp[i] << ' ';
-    return 0;
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	int n;
+	int dx[]={1,-1};
+	cin >> n;
+	vector<ll> a(n+1),b(n+1),d(n+2,INF);
+	vector<vector<pli>> graph(n+2);
+	for(int i=1;i<=n;i++) cin >> a[i];
+	for(int i=1;i<=n;i++){
+		cin >> b[i];
+		for(int j=0;j<2;j++){
+			int nx=i+dx[j]*a[i];
+			if(n<nx || nx<1) graph[n+1].push_back({b[i],i});
+			else graph[nx].push_back({b[i],i});
+		}
+	}
+	priority_queue<pli,vector<pli>,greater<pli>> pq;
+	d[n+1]=0;
+	pq.push({0,n+1});
+	while(!pq.empty()){
+		int cur=pq.top().S;
+		ll dis=pq.top().F;
+		pq.pop();
+		for(int i=0;i<graph[cur].size();i++){
+			int next=graph[cur][i].S;
+			ll cost=dis+graph[cur][i].F;
+			if(cost<d[next]){
+				d[next]=cost;
+				pq.push({cost,next});
+			}
+		}
+	}
+	for(int i=1;i<=n;i++) cout << d[i] << ' ';
+	return 0;
 }
