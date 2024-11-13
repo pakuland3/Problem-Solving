739760e392f9295d986d7d6383952b079143dbbe
pakuland3 <pakuland_1@naver.com>
2024-11-13 20:07:36 +0900

    1113

--- c++/7869.cpp
+++ c++/7869.cpp
@@ -1,42 +1,57 @@
 #include <bits/stdc++.h>
+#define PI 3.14159265358979
 typedef long long ll;
+typedef long double ld;
 using namespace std;
 
 struct p{
-    double x,y,r;
+    ld x,y,r;
 };
 
-double dis(p a, p b){
+ld dis(p a, p b){
     return sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y));
 }
 
-double solve(p a, p b){
-    double d=dis(a,b);
-    double bot=2*d*a.r;
-    double t=acos((d*d+a.r*a.r-b.r*b.r)/bot);
-    double S1=a.r*a.r*t/2;
-    double S2=a.r*a.r*sin(t)*cos(t);
+ld theta(p a, p b){
+    ld d=dis(a,b);
+    ld bot=(ld)2*d*a.r;
+    ld t=acos((d*d+a.r*a.r-b.r*b.r)/bot);
+    return t;
+}
+
+ld solve(p a, p b){
+    ld t=theta(a,b);
+    ld S1=a.r*a.r*t/(ld)2;
+    ld S2=a.r*a.r*sin(t)*cos(t);
     return S1-S2;
 }
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     cout.precision(3);
-    cout.fixed;
+    cout << fixed;
     p a,b;
     cin >> a.x >> a.y >> a.r >> b.x >> b.y >> b.r;
-    if(a.r==0 || b.r==0 || dis(a,b)>=a.r+b.r){
+    ld d=dis(a,b);
+    if(a.r==0 || b.r==0 || d>=a.r+b.r){
         cout << 0;
         return 0;
     }
-    double ans;
-    if(a.r+dis(a,b)<=b.r) ans=a.r*a.r*3.14159265358979;
-    else if(b.r+dis(a,b)<=a.r) ans=b.r*b.r*3.14159265358979;
+    ld ans=-1;
+    if(d<b.r){
+        ld t=theta(b,a);
+        ans=b.r*a.r/(ld)2+b.r*b.r*(t-sin(t));
+    }
+    else if(d<a.r){
+        ld t=theta(b,a);
+        ans=b.r*a.r/(ld)2+a.r*a.r*(t-sin(t));
+    }
+    else if(a.r+d<=b.r) ans=a.r*a.r*PI;
+    else if(b.r+d<=a.r) ans=b.r*b.r*PI;
     else ans=solve(a,b)+solve(b,a);
-    ans=ans*10000;
-    ll tmp=ans;
-    if(tmp%10>4) tmp+=10;
-    ans=tmp/10000;
-    cout << ans; 
+    ans*=1000;
+    ans=round(ans);
+    ans/=1000;
+    cout << ans;
     return 0;
 }
