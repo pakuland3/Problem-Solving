117b6a9331369cac4cbcc258e2c19e7ce5a9cacc
pakuland3 <pakuland4@gmail.com>
2024-11-28 20:08:17 +0900

    1128

---
+++ c++/30010.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << n << ' ';
+    for(int i=1;i<n;i++) cout << i << ' ';
+    return 0;
+}
