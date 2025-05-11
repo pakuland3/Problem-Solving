175aeb283cf60678dbb2ae869812bbd74fb9d52d
pakuland3 <pakuland4@gmail.com>
2025-05-12 00:11:40 +0900

    0512

---
+++ c++/28701.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    cout << n*(n+1)/2 << '\n' << n*n*(n+1)*(n+1)/4 << '\n' << n*n*(n+1)*(n+1)/4;
+    return 0;
+}
