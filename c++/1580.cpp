64b865558c4e65807a36e3c5fb9b79fdd06a1354
pakuland3 <pakuland4@gmail.com>
2025-06-16 13:02:10 +0900

    0616

--- c++/1580.cpp
+++ c++/1580.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
-#define fi first
-#define se second
+#define x first
+#define y second
+#define C(k) (n<=k.x || k.x<0 || m<=k.y || k.y<0)
+#define D(k) (g[k.x][k.y]=='X')
 typedef long long ll;
 using namespace std;
 
-int dx[]={0,-1,-1,-1,0,1,1,1};
-int dy[]={1,1,0,-1,-1,-1,0,1};
 int n,m;
-char g[21][21];
-int ax,ay,bx,by;
-int ad[21][21];
-int bd[21][21];
+char g[20][20];
+int d[20][20][20][20];
+int dx[]={0,-1,-1,-1,0,1,1,1,0};
+int dy[]={0,-1,0,1,1,1,0,-1,-1};
 
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
+    fill(d[0][0][0],d[20][0][0],999);
     cin >> n >> m;
+    int ax,ay,bx,by;
     for(int i=0;i<n;i++) for(int j=0;j<m;j++){
         cin >> g[i][j];
         if(g[i][j]=='A'){
@@ -26,8 +28,29 @@ int main(){
             by=j;
         }
     }
-    queue<pair<pair<int,int>,int>> q;
-    q.push({{ax,ay},0});
-    q.push({{bx,by},0});
+    queue<pair<pair<int,int>,pair<int,int>>> q;
+    q.push({{ax,ay},{bx,by}});
+    d[ax][ay][bx][by]=0;
+    while(q.size()){
+        pair<int,int> a=q.front().first;
+        pair<int,int> b=q.front().second;
+        int c=d[a.x][a.y][b.x][b.y];
+        q.pop();
+        for(int i=0;i<9;i++){
+            pair<int,int> na={a.x+dx[i],a.y+dy[i]};
+            if(C(na) || D(na)) continue;
+            for(int j=0;j<9;j++){
+                pair<int,int> nb={b.x+dx[j],b.y+dy[j]};
+                if(C(nb) || D(nb)) continue;
+                if(na.x==nb.x && na.y==nb.y) continue;
+                if(na.x==b.x && na.y==b.y && nb.x==a.x && nb.y==a.y) continue;
+                if(d[na.x][na.y][nb.x][nb.y]>c+1){
+                    q.push({na,nb});
+                    d[na.x][na.y][nb.x][nb.y]=c+1;
+                }
+            }
+        }
+    }
+    cout << (d[bx][by][ax][ay]==999?-1:d[bx][by][ax][ay]);
     return 0;
 }
